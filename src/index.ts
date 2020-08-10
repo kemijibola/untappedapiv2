747ff0b7dee1917e694aa016433f17aeac3c0d0c
kemijibzz <kemijibbola@gmail.com>
2020-08-10 13:00:10 +0100

    implemented master cluster for cron jobs

--- src/index.ts
+++ src/index.ts
@@ -14,19 +14,13 @@ import cors from "cors";
 // import SocketIo = require('./socket/SocketIo');
 import { userFilterJob, contestSettlement } from "./utils/CronJob";
 import { PlatformError } from "./utils/error";
+/// <reference path="typings/tsd.d.ts" />
+import * as cluster from "cluster";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-// app.use(function(req: Request, res: Response, next: NextFunction) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// });
 
 app.use(AppRouter.getInstance);
 
@@ -50,9 +44,12 @@ process.on("unhandledRejection", function (err) {
 const port = config.PORT || 5000;
 app.set("port", port);
 
-userFilterJob();
+if (cluster.isMaster) {
+  // only master should run cron job
+  userFilterJob();
 
-contestSettlement();
+  contestSettlement();
+}
 
 app.listen(port, () => {
   console.log(`Untapped Pool app successfully started on ${port}`);
